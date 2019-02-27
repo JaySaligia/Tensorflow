83d4d9113a5fc2672b157d2882575dd6a7cdd5bf
jaysaligia <773294157@qq.com>
2019-02-27 12:39:25 +0800

    去噪自编码器

--- Tensorflow/t0.py
+++ Tensorflow/t0.py
@@ -1,6 +1,7 @@
 #softmax regression识别minst
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+
 minst = input_data.read_data_sets("MNIST_data/", one_hot = True)
 sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, [None, 784])
--- Tensorflow/t1.py
+++ Tensorflow/t1.py
@@ -1,7 +1,8 @@
+#去噪自编码器
 import numpy as np
 import sklearn.preprocessing as prep
 import tensorflow as tf
-from tensorflow.examples.tuturials.mnist import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 
 #去噪自编码器
 def xavier_init(fan_in, fan_out, constant = 1):#Xaiver initialization
@@ -10,23 +11,23 @@ def xavier_init(fan_in, fan_out, constant = 1):#Xaiver initialization
     return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high, dtype=tf.float32)
 
 class AdditiveGaussianNoiseAutoencoder(object):
-    def __init__(self, n_input, n_hidden, transfer_function=tf.nn.softplus, optimizer=tf.train.AdamOptimizer(), scale=1.0):
+    def __init__(self, n_input, n_hidden, transfer_function=tf.nn.softplus, optimizer=tf.train.AdamOptimizer(), scale=0.1):
         self.n_input = n_input
         self.n_hidden = n_hidden
         self.transfer = transfer_function
         self.scale = tf.placeholder(tf.float32)
         self.training_scale = scale
-        network_weight = self._initialize_weights()
+        network_weights = self._initialize_weights()
         self.weights = network_weights
         #network structure
         self.x = tf.placeholder(tf.float32, [None, self.n_input])
-        self.hiddeng = self.transfer(tf.add(tf.matmul(
+        self.hidden = self.transfer(tf.add(tf.matmul(
             self.x + scale * tf.random_normal((n_input, )),
             self.weights['w1']), self.weights['b1']))
         self.reconstruction = tf.add(tf.matmul(self.hidden,
                                                self.weights['w2']), self.weights['b2'])
         #loss func
-        self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.substract(
+        self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(
             self.reconstruction, self.x), 2.0))
         self.optimizer = optimizer.minimize(self.cost)
         init = tf.global_variables_initializer()
@@ -36,9 +37,9 @@ class AdditiveGaussianNoiseAutoencoder(object):
     def _initialize_weights(self):
         all_weights = dict();
         all_weights['w1'] = tf.Variable(xavier_init(self.n_input, self.n_hidden))
-        all_weights['b1'] = tf.Variable(tf.zeros[self.n_hidden], dtype=tf.float32)
-        all_weights['w2'] = tf.Variable(tf.zeros[self.n_hidden, self.n_input], dtype=tf.float32)
-        all_weights['b2'] = tf.Variable(tf.zeros[self.n_hidden], dtype=tf.float32)
+        all_weights['b1'] = tf.Variable(tf.zeros([self.n_hidden], dtype=tf.float32))
+        all_weights['w2'] = tf.Variable(tf.zeros([self.n_hidden, self.n_input], dtype=tf.float32))
+        all_weights['b2'] = tf.Variable(tf.zeros([self.n_input], dtype=tf.float32))
         return all_weights
 
     def partial_fit(self, X):
@@ -66,12 +67,12 @@ class AdditiveGaussianNoiseAutoencoder(object):
         return self.sess.run(self.weights['b1'])
 
 #test MNIST
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
 def standard_scale(X_train, X_test):
     preprocessor = prep.StandardScaler().fit(X_train)
     X_train = preprocessor.transform(X_train)
-    X_test = preprocessor.transform(X_teat)
+    X_test = preprocessor.transform(X_test)
     return X_train, X_test
 
 def get_random_block_from_data(data, batch_size):
